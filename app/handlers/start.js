2e27df70c324990aa8ebba18175a56d673ff36d1
nati <natig.88x@gmail.com>
2017-06-12 19:02:16 +0300

    save user on get started

--- app/handlers/start.js
+++ app/handlers/start.js
@@ -1,6 +1,26 @@
+const User = require('./../models/user');
+const mainMenu = require('./../expo/main-menu');
+
+/* eslint-disable no-param-reassign */
 module.exports = (payload, chat) => {
-  chat.getUserProfile().then((user) => {
-    chat.say(`Hello, ${user.first_name} 😀 I'm the ICT Expo Ethiopia 2017 Bot 🤖 I'll be your assistant for the event.\nHere are some of the things i can help you with!`);
-    // TODO send menu cards
+  chat.getUserProfile().then((userProfile) => {
+    userProfile.userId = payload.sender.id;
+    userProfile.startedOn = payload.timestamp;
+    const user = new User(userProfile);
+    user.save((err) => {
+      if (err) {
+        if (err.errors.userId.kind === 'unique') {
+          chat.say(`Welcome back ${user.first_name}😜`, { typing: true }).then(() => {
+            mainMenu(chat);
+          });
+        } else { // TODO handle signup error
+          console.log('error:', err.message);
+        }
+      } else { // on user saved
+        chat.say(`Hello, ${user.first_name} 😀 I'm the ICT Expo Ethiopia 2017 Bot 🤖 I'll be your assistant for the event.\nHere are some of the things i can help you with!`, { typing: true }).then(() => {
+          mainMenu(chat);
+        });
+      }
+    });
   });
 };
